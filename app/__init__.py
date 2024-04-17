d654dbd13c4f29fb93780f6849e5b206e60a362c
Mahadika <93863120+SecretXpawn@users.noreply.github.com>
2024-04-17 07:51:33 +0700

    1

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,3 @@
 from flask import Flask
-
-# Initialize Flask application
 app = Flask(__name__)
-
-# Import routes module to ensure routes are registered with the application
 from app import routes
